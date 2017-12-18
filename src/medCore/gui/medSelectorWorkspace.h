3c1e733a00ed145825047d2fc87ccc13237f9b3d
mathilde.merle <mathilde.merle@ihu-liryc.fr>
2017-12-18 11:22:44 +0100

    [medToolBox] standardize on process success

--- src/medCore/gui/medSelectorWorkspace.h
+++ src/medCore/gui/medSelectorWorkspace.h
@@ -35,7 +35,7 @@ public:
     medSelectorToolBox *selectorToolBox();
 
 protected slots:
-    virtual void onProcessSuccess(){}
+    void importProcessOutput();
 
 private:
     medSelectorWorkspacePrivate *d;
