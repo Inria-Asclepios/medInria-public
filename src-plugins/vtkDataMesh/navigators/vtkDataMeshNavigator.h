e577d1d319e63c8c882d383dbf1a7b9e12c0b733
mathilde.merle <mathilde.merle@ihu-liryc.fr>
2018-02-09 10:36:30 +0100

    [Screenshots] protected slots

--- src-plugins/vtkDataMesh/navigators/vtkDataMeshNavigator.h
+++ src-plugins/vtkDataMesh/navigators/vtkDataMeshNavigator.h
@@ -40,6 +40,8 @@ public slots:
 
     void enableDepthPeeling(bool);
     void setBackgroundColor(const QString &color);
+
+protected slots:
     void setBackgroundColor(QColor color);
 
 protected:
