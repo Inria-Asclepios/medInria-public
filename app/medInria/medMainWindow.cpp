746513942d838555bf1e3e846ef39072c2bf8cc3
mathilde.merle <mathilde.merle@ihu-liryc.fr>
2018-02-16 10:50:46 +0100

    [Std] add a grey icon for adjust_container for understanding of grey/white buttons

--- app/medInria/medMainWindow.cpp
+++ app/medInria/medMainWindow.cpp
@@ -231,6 +231,7 @@ medMainWindow::medMainWindow ( QWidget *parent ) : QMainWindow ( parent ), d ( n
 
     QIcon adjustIcon;
     adjustIcon.addPixmap(QPixmap(":icons/adjust_size.png"),QIcon::Normal);
+    adjustIcon.addPixmap(QPixmap(":icons/adjust_size_grey.png"),QIcon::Disabled);
     d->adjustSizeButton = new QToolButton(this);
     d->adjustSizeButton->setIcon(adjustIcon);
     d->adjustSizeButton->setObjectName("adjustSizeButton");
