a0852062732104e32e55570de3f558891b46953f
mjuhoor <mehdi.juhoor@inria.fr>
2020-03-30 12:17:06 +0200

    Now compiles music-plugins, fix tetgen external project.

--- superbuild/projects_modules/tetgen.cmake
+++ superbuild/projects_modules/tetgen.cmake
@@ -50,6 +50,8 @@ set(cmake_args
 ## #############################################################################
 ## Add external-project
 ## #############################################################################
+epComputPath(${ep})
+
 ExternalProject_Add(${ep}
   PREFIX ${EP_PATH_SOURCE}
   SOURCE_DIR ${EP_PATH_SOURCE}/${ep}
