e7d0a43eef190a34d5c6caa58a997228f8cbfa41
ARRIEULA Andony <andony.arrieula@inria.fr>
2021-02-16 10:47:24 +0100

    [ICP] clean unused packages

--- src/plugins/legacy/iterativeClosestPoint/iterativeClosestPointProcess.h
+++ src/plugins/legacy/iterativeClosestPoint/iterativeClosestPointProcess.h
@@ -17,9 +17,6 @@
 #include <medAbstractData.h>
 #include <medAbstractProcessLegacy.h>
 
-#include <vtkSmartPointer.h>
-#include <vtkMatrix4x4.h>
-
 class iterativeClosestPointProcessPrivate;
 
 class ITERATIVECLOSESTPOINTPLUGIN_EXPORT iterativeClosestPointProcess : public medAbstractProcessLegacy
