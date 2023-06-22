4e3ccf2f5e58fba0b569163c0330fdc506d082e2
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2023-06-22 16:47:42 +0200

    [v3.3.2] branch music plugins v3.3.0 not master

--- superbuild/projects_modules/music-plugins.cmake
+++ superbuild/projects_modules/music-plugins.cmake
@@ -34,7 +34,7 @@ function(music_plugins_project)
     if (NOT USE_SYSTEM_${external_project})
 
         set(git_url ${GITHUB_PREFIX}Inria-Asclepios/music.git)
-        set(git_tag master)
+        set(git_tag v3.3.0)
 
         set(cmake_args
             ${ep_common_cache_args}
