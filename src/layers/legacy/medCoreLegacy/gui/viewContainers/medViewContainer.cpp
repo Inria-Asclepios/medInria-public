9684448736263c1474550973d2256fac4a0a7bdb
mathildemerle <mathilde.merle@ihu-liryc.fr>
2026-03-25 16:52:13 +0100

    [Container] add object name to label in view (#923)

--- src/layers/legacy/medCoreLegacy/gui/viewContainers/medViewContainer.cpp
+++ src/layers/legacy/medCoreLegacy/gui/viewContainers/medViewContainer.cpp
@@ -333,6 +333,7 @@ void medViewContainer::createdDefaultWidget()
     d->defaultWidget = new QWidget;
     d->defaultWidget->setObjectName("defaultWidget");
     QLabel *defaultLabel = new QLabel(tr("Drag'n drop series/study here from the left panel or:"));
+    defaultLabel->setObjectName("defaultLabel");
     QPushButton *openButton  = new QPushButton(tr("Open a file from your system"));
     openButton->setObjectName("openButton");
     QPushButton *sceneButton = new QPushButton(tr("Open a scene from your system"));
