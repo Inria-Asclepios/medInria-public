c0fcf431cc436cf55da3b3a55c634af74ccfa7b8
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2026-04-23 10:59:16 +0200

    [Filtering dtk] removes duplicated with Filtering and unused workspace

--- src/layers/medWidgets/medWidgets.h
+++ src/layers/medWidgets/medWidgets.h
@@ -14,37 +14,11 @@
 
 #include <medProcessPresenterFactory.h>
 
-#include <medAbstractOpeningImageProcessPresenter.h>
-#include <medAbstractClosingImageProcessPresenter.h>
-#include <medAbstractDilateImageProcessPresenter.h>
-#include <medAbstractErodeImageProcessPresenter.h>
-
-#include <medAbstractAddImageProcessPresenter.h>
-#include <medAbstractSubtractImageProcessPresenter.h>
-#include <medAbstractMultiplyImageProcessPresenter.h>
-#include <medAbstractDivideImageProcessPresenter.h>
-
 #include <medAbstractMaskImageProcessPresenter.h>
 #include <medAbstractDWIMaskingProcessPresenter.h>
 #include <medAbstractDiffusionModelEstimationProcessPresenter.h>
 #include <medAbstractDiffusionScalarMapsProcessPresenter.h>
 #include <medAbstractTractographyProcessPresenter.h>
-
-#include <medAbstractSingleFilterOperationProcessPresenter.h>
-#include <medAbstractAddFilterProcessPresenter.h>
-#include <medAbstractDivideFilterProcessPresenter.h>
-#include <medAbstractGaussianFilterProcessPresenter.h>
-#include <medAbstractInvertFilterProcessPresenter.h>
-#include <medAbstractMedianFilterProcessPresenter.h>
-#include <medAbstractMultiplyFilterProcessPresenter.h>
-#include <medAbstractNormalizeFilterProcessPresenter.h>
-#include <medAbstractShrinkFilterProcessPresenter.h>
-#include <medAbstractSubtractFilterProcessPresenter.h>
-#include <medAbstractWindowingFilterProcessPresenter.h>
-#include <medAbstractImageDenoisingProcessPresenter.h>
-#include <medAbstractRelaxometryEstimationProcessPresenter.h>
-#include <medAbstractSymmetryPlaneAlignmentProcessPresenter.h>
-#include <medAbstractBiasCorrectionProcessPresenter.h>
 
 #include <medWidgetsExport.h>
 
@@ -58,109 +32,6 @@ namespace medWidgets
     namespace generic
     {
         MEDWIDGETS_EXPORT medAbstractProcessPresenterFactory& presenterFactory();
-    }
-
-    namespace morphomathOperation
-    {
-        namespace erodeImage
-        {
-            MEDWIDGETS_EXPORT medAbstractErodeImageProcessPresenterFactory& presenterFactory();
-        }
-        namespace dilateImage
-        {
-            MEDWIDGETS_EXPORT medAbstractDilateImageProcessPresenterFactory& presenterFactory();
-        }
-        namespace openingImage
-        {
-            MEDWIDGETS_EXPORT medAbstractOpeningImageProcessPresenterFactory& presenterFactory();
-        }
-        namespace closingImage
-        {
-            MEDWIDGETS_EXPORT medAbstractClosingImageProcessPresenterFactory& presenterFactory();
-        }
-    }
-
-    namespace arithmeticOperation
-    {
-        namespace addImage
-        {
-            MEDWIDGETS_EXPORT medAbstractAddImageProcessPresenterFactory& presenterFactory();
-        }
-
-        namespace subtractImage
-        {
-            MEDWIDGETS_EXPORT medAbstractSubtractImageProcessPresenterFactory& presenterFactory();
-        }
-
-        namespace multiplyImage
-        {
-            MEDWIDGETS_EXPORT medAbstractMultiplyImageProcessPresenterFactory& presenterFactory();
-        }
-
-        namespace divideImage
-        {
-            MEDWIDGETS_EXPORT medAbstractDivideImageProcessPresenterFactory& presenterFactory();
-        }
-    }
-
-    namespace singleFilterOperation
-    {
-        namespace addFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractAddFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace divideFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractDivideFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace gaussianFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractGaussianFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace invertFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractInvertFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace medianFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractMedianFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace multiplyFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractMultiplyFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace normalizeFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractNormalizeFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace shrinkFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractShrinkFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace subtractFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractSubtractFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace windowingFilter
-        {
-            MEDWIDGETS_EXPORT medAbstractWindowingFilterProcessPresenterFactory& presenterFactory();
-        }
-        namespace imageDenoising
-        {
-            MEDWIDGETS_EXPORT medAbstractImageDenoisingProcessPresenterFactory& presenterFactory();
-        }
-        namespace relaxometryEstimation
-        {
-            MEDWIDGETS_EXPORT medAbstractRelaxometryEstimationProcessPresenterFactory& presenterFactory();
-        }
-        namespace symmetryAlignment
-        {
-            MEDWIDGETS_EXPORT medAbstractSymmetryPlaneAlignmentProcessPresenterFactory& presenterFactory();
-        }
-        namespace biasCorrection
-        {
-           MEDWIDGETS_EXPORT medAbstractBiasCorrectionProcessPresenterFactory& presenterFactory();
-        }
     }
 
     namespace maskImage
