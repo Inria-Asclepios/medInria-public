59ac44ef845b38be859c11cda6e90e039e5e0494
Mathilde Merle <mathilde.merle@ihu-liryc.fr>
2026-06-25 11:26:40 +0200

    [4.1] switch music-plugins to master branch

--- superbuild/projects_modules/music-plugins.cmake
+++ superbuild/projects_modules/music-plugins.cmake
@@ -42,7 +42,7 @@ function(music_plugins_project)
     if (NOT USE_SYSTEM_${external_project})
 
         set(git_url ${GITHUB_PREFIX}Inria-Asclepios/music.git)
-        set(git_tag 4.1)
+        set(git_tag master)
 
 ## #############################################################################
 ## Add specific cmake arguments for configuration step of the project
